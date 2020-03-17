f4aea3c7d0c4b1ed05e1fe2e92f581f9ea1cd6a5
way2key <olivier_dancona@hotmail.com>
2020-03-17 08:22:54 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { TeacherLoginComponent } from './teacher/teacher-login/teacher-login.component';
+import { TeacherMainComponent } from './teacher/teacher-main/teacher-main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/student', pathMatch: 'full' },
+  { path: 'student', component: StudentComponent },
+  { path: 'teacher/dashboard', component: TeacherMainComponent },
+  { path: 'teacher/login', component: TeacherLoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+import { StudentComponent } from './student/student.component';
+
+import { TeacherLoginComponent } from './teacher/teacher-login/teacher-login.component';
+import { TeacherMainComponent } from './teacher/teacher-main/teacher-main.component';
+import { TeacherStudentComponent } from './teacher/teacher-student/teacher-student.component';
+import { TeacherStatComponent } from './teacher/teacher-stat/teacher-stat.component';
+import { TeacherSettingComponent } from './teacher/teacher-setting/teacher-setting.component';
+import { TeacherInfoComponent } from './teacher/teacher-info/teacher-info.component';
+import { TeacherDashboardComponent } from './teacher/teacher-dashboard/teacher-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    TeacherLoginComponent,
+    TeacherMainComponent,
+    TeacherStudentComponent,
+    TeacherStatComponent,
+    TeacherSettingComponent,
+    TeacherInfoComponent,
+    TeacherDashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
