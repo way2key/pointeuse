358ea914d7d5fb24fa1afc5bf9492da7495dcb9c
scorpolix <61693808+scorpolix@users.noreply.github.com>
2020-03-20 22:51:08 +0100

    Statut In/out

--- src/app/student/student-info/student-info.component.ts
+++ src/app/student/student-info/student-info.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentInfoComponent implements OnInit {
 
+  studentStatus = false;
+
   constructor() { }
 
   ngOnInit(): void {
