8accfe4c922165f705523d0ed6a650e23c4430a2
scorpolix <61693808+scorpolix@users.noreply.github.com>
2020-04-07 22:00:21 +0200

    Sort order statistics
    Choisir l'ordre d'affichage des stagiaires pour trier dans teacher/hist/stat

--- src/app/teacher/teacher-hist-stat/teacher-hist-stat.component.ts
+++ src/app/teacher/teacher-hist-stat/teacher-hist-stat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TeacherStudentService } from '../teacher-student.service';
 import {FormGroup, FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
@@ -10,18 +11,21 @@ import {map, startWith} from 'rxjs/operators';
 })
 export class TeacherHistStatComponent implements OnInit {
 
-  myControl = new FormControl();
+  students = [];
+
+  shownStudents = [];
+
+  sortCriteria = new FormControl('alphabetical');
+
+
+  myControlSearch = new FormControl();
   options: string[] = ['One', 'Two', 'Three'];
   filteredOptions: Observable<string[]>;
 
-  constructor() { }
+  constructor(private teacherStudentService: TeacherStudentService,) { }
 
   ngOnInit(): void {
-    this.filteredOptions = this.myControl.valueChanges
-    .pipe(
-      startWith(''),
-      map(value => this._filter(value))
-    );
+    this.getStudents();
   }
 
   private _filter(value: string): string[] {
@@ -32,6 +36,95 @@ export class TeacherHistStatComponent implements OnInit {
 
   getStudent() {
     alert("you got one!");
+  }
+
+  getStudents(): void {
+    this.students = [];
+    this.teacherStudentService.getAllStudents()
+    .subscribe((dbStudents) => {
+      this.students = dbStudents;
+      this.students.sort(this.sortOrder());
+      this.shownStudents = this.students;
+      this.autocompleteFill();
+      this.filteredOptions = this.myControlSearch.valueChanges
+      .pipe(
+        startWith(''),
+        map((value) => {
+          this.shownStudents = [];
+          let studentNames = this._filter(value);
+          studentNames.forEach(studentName => {
+            this.students.forEach(student => {
+              if(studentName === (student.firstname + ' ' + student.lastname)){
+                this.shownStudents.push(student);
+              };
+            })
+          });
+          return studentNames;
+        })
+      );
+    });
+  }
+
+  autocompleteFill() {
+    this.options = [];
+    for(let student of this.students) {
+      let name = student.firstname + ' ' + student.lastname;
+      this.options.push(name);
+    }
+  }
+
+  sortOrder() {
+    if (this.sortCriteria.value === 'alphabetical') {
+      return this.alphabeticalSort();
+    } else if (this.sortCriteria.value === 'lowToHigh') {
+      return this.lowToHighSort();
+    } else if (this.sortCriteria.value === 'highToLow') {
+      return this.highToLowSort();
+    }
+  }
+
+  alphabeticalSort() {
+    let lastname = 'lastname';
+    return function(a, b) {
+      if(a[lastname] > b[lastname]) {
+        return 1;
+      } else if (a[lastname] < b[lastname]) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+  }
+
+  lowToHighSort() {
+    let performedTime = 'performedTime';
+    return function(a, b) {
+      if(a[performedTime] > b[performedTime]) {
+        return 1;
+      } else if (a[performedTime] < b[performedTime]) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+  }
+
+  highToLowSort() {
+    let performedTime = 'performedTime';
+    return function(a, b) {
+      if(a[performedTime] > b[performedTime]) {
+        return -1;
+      } else if (a[performedTime] < b[performedTime]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  }
+
+  reloadStudents() {
+    this.getStudents();
+    this.shownStudents.sort(this.sortOrder());
   }
 
 }
